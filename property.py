2485b9916147d62e9ee0f0b1a729633702dcb312
Asher Khan <m.asherkhan657@gmail.com>
2025-03-01 22:03:16 -0600

    Added property information to json file and added some logic to property.py

--- property.py
+++ property.py
@@ -1,21 +1,37 @@
+from typing import List
+from enum import Enum, auto
+
 from board_space import BoardSpace
 from space_type import SpaceType
+
+
+class PropertyColor(Enum):
+    BROWN = auto()
+    SKYBLUE = auto()
+    PURPLE = auto()
+    ORANGE = auto()
+    RED = auto()
+    YELLOW = auto()
+    GREEN = auto()
+    BLUE = auto()
+
+
+class PropertyType(Enum):
+    LAND = auto()
+    RAILROAD = auto()
+    UTILITY = auto()
 
 
 class Property(BoardSpace):
     def __init__(self, name: str,
                  position: int,
                  purchase_cost: int,
-                 color: str,
-                 rent: int,
-                 rent_1_house: int,
-                 rent_2_house: int,
-                 rent_3_house: int,
-                 rent_4_house: int,
+                 color: PropertyColor,
+                 property_type: PropertyType,
+                 rent: List[int],
                  rent_hotel: int,
                  mortgage_value: int,
                  house_cost: int,
-                 hotel_cost: int,
                  is_owned: bool = False,
                  is_mortgaged: bool = False,
                  num_houses: int = 0,
@@ -23,33 +39,30 @@ class Property(BoardSpace):
         super().__init__(name, position, SpaceType.PROPERTY)
         self.purchase_cost = purchase_cost
         self.color = color
+        self.property_type = property_type
         self.rent = rent
-        self.rent_1_house = rent_1_house
-        self.rent_2_house = rent_2_house
-        self.rent_3_house = rent_3_house
-        self.rent_4_house = rent_4_house
         self.rent_hotel = rent_hotel
         self.mortgage_value = mortgage_value
+        self.mortgage_cost = self.mortgage_value * 1.1
         self.house_cost = house_cost
-        self.hotel_cost = hotel_cost
         self.is_owned = is_owned
         self.is_mortgaged = is_mortgaged
         self.num_houses = num_houses
         self.num_hotels = num_hotels
 
-    def get_rent(self):
-        if self.is_mortgaged or not self.is_owned:
-            return 0
-        if self.num_houses == 0 and self.num_hotels == 0:
-            return self.rent
-        elif self.num_hotels >= 1:
-            return self.rent_hotel
-        elif self.num_houses == 1:
-            return self.rent_1_house
-        elif self.num_houses == 2:
-            return self.rent_2_house
-        elif self.num_houses == 3:
-            return self.rent_3_house
-        elif self.num_houses == 4:
-            return self.rent_4_house
-
+    def get_rent(self, property_type: PropertyType, dice_roll: int):
+        if property_type == PropertyType.LAND:
+            if self.is_mortgaged or not self.is_owned:
+                return 0
+            if self.num_hotels > 0:
+                return self.rent_hotel
+            else:
+                return self.rent[self.num_houses]
+        elif property_type == PropertyType.RAILROAD:
+            # TODO: get number of railroads owned and return value
+            num_railroads = None
+            return self.rent[num_railroads - 1]
+        else:
+            # TODO: get number of utilities owned and return value
+            num_utilities = None
+            return dice_roll * self.rent[num_utilities]
