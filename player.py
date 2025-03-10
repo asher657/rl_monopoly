92c476064a8956c2ef9730656c7a713c688474ce
Asher Khan <m.asherkhan657@gmail.com>
2025-03-09 21:26:01 -0500

    Started project 3 and completed Sarsa agent

--- player.py
+++ player.py
@@ -1,6 +1,17 @@
+import random
+
 from board_space import BoardSpace
+from property import Property, PropertyType, PropertyColor, PROPERTY_COLOR_COUNTS
 from space_type import SpaceType
 
+initial_color_group_counts = {PropertyColor.BROWN: 0,
+                              PropertyColor.SKYBLUE: 0,
+                              PropertyColor.PURPLE: 0,
+                              PropertyColor.ORANGE: 0,
+                              PropertyColor.RED: 0,
+                              PropertyColor.YELLOW: 0,
+                              PropertyColor.GREEN: 0,
+                              PropertyColor.BLUE: 0}
 
 class Player:
     def __init__(self,
@@ -8,6 +19,25 @@ class Player:
                  money: int = 1500):
         self.curr_space = curr_space
         self.money = money
-        self.color_sets = set()
-        # TODO: does owned properties go here or board
+        self.owned_color_groups = set()
+        self.color_group_counts = initial_color_group_counts
+        self.owned_properties = set()
+
+    def make_move(self):
+        move = random.randint(1, 12)
+        self.curr_space += move # TODO: wrap around if they get to GO
+        # TODO: handle space logic
+
+    def buy_property(self, bought_property: Property):
+        self.owned_properties.add(bought_property)
+        if bought_property.property_type == PropertyType.LAND:
+            self.set_color_groups(bought_property)
+
+    def set_color_groups(self, bought_property):
+        property_color = bought_property.color
+        self.color_group_counts[property_color] += 1
+        if self.color_group_counts[property_color] == PROPERTY_COLOR_COUNTS[property_color]:
+            self.owned_color_groups.add(property_color)
+
+
 
