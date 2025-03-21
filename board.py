47f3c72368005f14436d72d4b1d644dd5e62abe3
Asher Khan <m.asherkhan657@gmail.com>
2025-03-21 17:35:50 -0500

    Minor changes such as reformatting file. Added reset method

--- board.py
+++ board.py
@@ -7,25 +7,24 @@ from board_space import BoardSpace
 from opponent import Opponent
 from space_type import SpaceType
 
-OPPONENT_MONEY_INDEX = 4
-PLAYER_MONEY_INDEX = 3
-RENT_INDEX = 2
-OPPONENT_SPACE_INDEX = 1
 HOUSES_INDEX = 0
+OPPONENT_SPACE_INDEX = 1
+RENT_INDEX = 2
+PLAYER_MONEY_INDEX = 3
+OPPONENT_MONEY_INDEX = 4
+
+INITIAL_NUM_HOUSES = 1
+
 
 class Board:
     def __init__(self, agent: Agent, default_cost: int = -10, debug: bool = False):
         self.agent = agent
         self.default_cost = default_cost
-        self.opponent = Opponent(debug)
+        self.debug = debug
+        self.opponent = Opponent(self.debug)
         self.board_positions = self._load_positions()
         self.bought_houses = {i: 0 for i in range(len(self.board_positions))}
         self.state = np.zeros(shape=(5, 40))
-        for idx, space in self.board_positions.items():
-            if space.space_type == SpaceType.PROPERTY:
-                self.state[RENT_INDEX, idx] = space.rent[1]
-        self.state[OPPONENT_SPACE_INDEX, self.opponent.curr_position] = 1
-        self.update_affordability_states()
 
     def _load_positions(self) -> Dict[int, BoardSpace]:
         with open('positions.json', 'r', encoding='utf-8') as f:
@@ -33,29 +32,35 @@ class Board:
 
         return {int(key): BoardSpace(**value) for key, value in json_data.items()}
 
-    def get_rent_cost(self, position: int):
-        position_data = self.board_positions[position]
-        return position_data.get_rent()
+    def reset(self, agent: Agent):
+        self.agent = agent
+        self.opponent = Opponent(self.debug)
+        for idx, space in self.board_positions.items():
+            if space.space_type == SpaceType.PROPERTY:
+                self.state[RENT_INDEX, idx] = space.rent[INITIAL_NUM_HOUSES]
+        self.state[OPPONENT_SPACE_INDEX, self.opponent.curr_position] = 1
+        self.update_affordability_states()
 
     def update_affordability_states(self) -> None:
         for idx, space in self.board_positions.items():
             if space.space_type == SpaceType.PROPERTY:
-                if self.agent.money > space.rent[1]:
-                    self.state[PLAYER_MONEY_INDEX,idx] = 1
+                if self.agent.money > space.rent[INITIAL_NUM_HOUSES]:
+                    self.state[PLAYER_MONEY_INDEX, idx] = 1
                 else:
-                    self.state[PLAYER_MONEY_INDEX,idx] = 0
-                if self.opponent.money > space.rent[1]:
-                    self.state[OPPONENT_MONEY_INDEX,idx] = 1
+                    self.state[PLAYER_MONEY_INDEX, idx] = 0
+                if self.opponent.money > space.rent[INITIAL_NUM_HOUSES]:
+                    self.state[OPPONENT_MONEY_INDEX, idx] = 1
                 else:
-                    self.state[OPPONENT_MONEY_INDEX,idx] = 0
+                    self.state[OPPONENT_MONEY_INDEX, idx] = 0
         return None
 
-    def update_state_space(self, agent: Agent, opponent: Opponent,opp_previous_pos, next_opponent_position: int, house_location: int) -> None:
+    def update_state_space(self, agent: Agent, opponent: Opponent, opp_previous_pos, next_opponent_position: int, house_location: int) -> None:
         self.update_affordability_states(agent, opponent)
-        self.state[OPPONENT_SPACE_INDEX,opp_previous_pos] = 0
-        self.state[OPPONENT_SPACE_INDEX,next_opponent_position] = 1
+        self.state[OPPONENT_SPACE_INDEX, opp_previous_pos] = 0
+        self.state[OPPONENT_SPACE_INDEX, next_opponent_position] = 1
         self.state[HOUSES_INDEX, house_location] = 1
         return None
+
     def execute_action(self, house_location: int):
         game_end = False
 
@@ -91,5 +96,5 @@ class Board:
                     game_end = True
 
         # update state space here
-        self.update_state_space(self.agent, self.opponent,opp_previous_pos, self.opponent.curr_position, house_location)
+        self.update_state_space(self.agent, self.opponent, opp_previous_pos, self.opponent.curr_position, house_location)
         return rent - house_cost, self.opponent.curr_position, game_end
