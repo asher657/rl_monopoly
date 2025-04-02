cff275c7017b59626e00991aeae245eddc3293cc
Asher Khan <m.asherkhan657@gmail.com>
2025-04-01 22:20:20 -0500

    Completed baseline agent. Tested with board. Also added logic to reset houses bought after opponent passes go

--- monopoly_logger.py
+++ monopoly_logger.py
@@ -14,17 +14,14 @@ def get_monopoly_logger(class_name, logging_level: str = 'info'):
 
     if not logger.hasHandlers():  # Prevent adding multiple handlers
         handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
         logger.addHandler(handler)
 
     if logging_level == 'info':
-        print(f'Setting logging level to: {logging_level}')
         logger.setLevel(logging.INFO)
     elif logging_level == 'debug':
-        print(f'Setting logging level to: {logging_level}')
         logger.setLevel(logging.DEBUG)
     else:
-        print(f'Logging level set to none. Not logging anything')
         logger.setLevel(logging.CRITICAL + 1)
 
     return logger
